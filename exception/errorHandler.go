c0c66a7f4217fa1030f5a67e581d7680cf3f54c6
fuadaghazada <fuad.aghazade@pashabank.az>
2021-09-12 14:17:43 +0400

    Small fix in error handler

--- exception/errorHandler.go
+++ exception/errorHandler.go
@@ -2,12 +2,15 @@ package exception
 
 import (
 	"encoding/json"
+	"github.com/fuadaghazada/ms-todoey-items/model"
 	"net/http"
 )
 
 func ErrorHandler(handler func(w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		err := handler(w, r)
+		w.Header().Add(model.ContentType, model.ApplicationJSON)
+
 		if err != nil {
 			switch e := err.(type) {
 			case *BadRequestError:
