c457f9e4919ca1867dcfaea883356145a0a515a9
fuadaghazada <fuad.aghazade@pashabank.az>
2021-09-18 11:39:03 +0400

    Refactor

--- service/itemService.go
+++ service/itemService.go
@@ -1,10 +1,12 @@
 package service
 
 import (
+	entity "github.com/fuadaghazada/ms-todoey-items/dao/model"
 	"github.com/fuadaghazada/ms-todoey-items/dao/repo"
 	"github.com/fuadaghazada/ms-todoey-items/exception"
 	itemMapper "github.com/fuadaghazada/ms-todoey-items/mapper"
 	"github.com/fuadaghazada/ms-todoey-items/model"
+	"github.com/go-pg/pg"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -63,19 +65,13 @@ func (i *itemService) GetUserItems(userID string) (*[]model.ItemDto, error) {
 func (i *itemService) GetUserItem(id int, userID string) (*model.ItemDto, error) {
 	log.Debugf("ActionLog.GetUserItem.start: Item#%v, User#%v", id, userID)
 
-	tx, _ := i.itemRepo.GetTransaction()
-	itemEntity, err := i.itemRepo.GetItemByIDAndUserID(tx, id, userID)
+	itemEntity, tx, err := i.getItem(id, userID)
 	if err != nil {
 		log.Errorf("ActionLog.GetUserItem.error: %v", err)
 		return nil, err
 	}
 
 	defer i.itemRepo.CloseTransaction(tx, err)
-
-	if itemEntity == nil {
-		log.Errorf("ActionLog.GetUserItem.error: Item not found %v", err)
-		return nil, exception.NewItemNotFoundError("error.item-not-found", "Item not found")
-	}
 
 	itemDTO := itemMapper.ToDTO(itemEntity)
 
@@ -87,24 +83,17 @@ func (i *itemService) GetUserItem(id int, userID string) (*model.ItemDto, error)
 func (i *itemService) UpdateItem(itemDto *model.CreateUpdateItemDto, itemID int, userID string) (*model.ItemDto, error) {
 	log.Debugf("ActionLog.UpdateItem.start: User#%v, Item#%v", userID, itemDto)
 
-	tx, _ := i.itemRepo.GetTransaction()
-
-	itemEntity, err := i.itemRepo.GetItemByIDAndUserID(tx, itemID, userID)
+	itemEntity, tx, err := i.getItem(itemID, userID)
 	if err != nil {
-		log.Errorf("ActionLog.UpdateItem.error: Cannot update item -> %v", err)
+		log.Errorf("ActionLog.UpdateItem.error: %v", err)
 		return nil, err
-	}
-
-	if itemEntity == nil {
-		log.Errorf("ActionLog.GetUserItem.error: Item not found %v", err)
-		return nil, exception.NewItemNotFoundError("error.item-not-found", "Item not found")
 	}
 
 	itemEntity = itemMapper.ToEntityUpdate(itemDto, itemEntity)
 
 	updatedItem, err := i.itemRepo.SaveItem(tx, itemEntity)
 	if err != nil {
-		log.Error("ActionLog.UpdateItem.error: Database error")
+		log.Errorf("ActionLog.UpdateItem.error: Database error %v", err)
 		return nil, exception.NewDatabaseError()
 	}
 
@@ -115,4 +104,25 @@ func (i *itemService) UpdateItem(itemDto *model.CreateUpdateItemDto, itemID int,
 	log.Debugf("ActionLog.CreateItem.end")
 
 	return resultItemDto, nil
+}
+
+func (i *itemService) getItem(id int, userID string) (*entity.ItemEntity, *pg.Tx, error) {
+	log.Debug("ActionLog.getItem.start")
+
+	tx, _ := i.itemRepo.GetTransaction()
+
+	itemEntity, err := i.itemRepo.GetItemByIDAndUserID(tx, id, userID)
+	if err != nil {
+		log.Errorf("ActionLog.getItem.error: %v", err)
+		return nil, tx, err
+	}
+
+	if itemEntity == nil {
+		log.Errorf("ActionLog.getItem.error: Item not found %v", err)
+		return nil, tx, exception.NewItemNotFoundError("error.item-not-found", "Item not found")
+	}
+
+	log.Debug("ActionLog.getItem.end")
+
+	return itemEntity, tx, nil
 }
